b76c7457dea36a8a8649fd9614971a59025aacb5
gaoshuaixiong <4850964@qq.com>
2016-03-30 01:04:46 -0700

    ADD: sriotest

--- ue_eth/lte_rlc_mac_module/fsmcore.c
+++ ue_eth/lte_rlc_mac_module/fsmcore.c
@@ -423,7 +423,7 @@ int fsm_mem_cmp(const void* dst_ptr, const void* src_ptr, size_t size)
 /* Print */
 size_t fsm_printf(const char* fmt,...)
 {
-	//return 0;
+	return 0;
 	int print_len;
 	va_list args;
 	va_start(args,fmt);
